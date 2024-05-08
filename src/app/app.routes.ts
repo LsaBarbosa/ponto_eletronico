ca5319f44caa99ac3d93311aba30cb522442c35e
LsaBarbosa <l.sant.anna.b@gmail.com>
2024-05-08 10:42:36 -0300

    Create employee

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,15 @@
 import { Routes } from '@angular/router';
 import {LoginComponent} from "./components/login/login.component";
+import {EmployeeComponent} from "./components/employee/employee.component";
 
 export const routes: Routes = [
   {
     path: '',
     component: LoginComponent,
+  },
+
+  {
+    path: 'colaborador',
+    component: EmployeeComponent,
   },
 ];
