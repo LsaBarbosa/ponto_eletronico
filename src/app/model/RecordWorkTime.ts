5e9935db3d376d3ab7f2aaa18e8cdd4dd5b94d78
LsaBarbosa <l.sant.anna.b@gmail.com>
2024-05-16 19:49:46 -0300

    refactor

--- src/app/model/RecordWorkTime.ts
+++ src/app/model/RecordWorkTime.ts
@@ -2,9 +2,11 @@ import {Employee} from "./Employee";
 
 export interface RecordWorkTime {
   id: number;
-  startOfWork: Date ;
-  endOfWork: Date  ;
-  timeWorkedInMinutes: string  ;
-  employee: Employee  ;
-  overtime:string;
+  startOfWork: string; // Alterado para string para corresponder ao formato do backend
+  endOfWork: string; // Alterado para string para corresponder ao formato do backend
+  startDate: string; // Adicionado para corresponder ao formato do backend
+  endDate: string; // Adicionado para corresponder ao formato do backend
+  timeWorked: string; // Mantido, desde que corresponda ao formato do backend
+  overtime: string; // Mantido, desde que corresponda ao formato do backend
+  employee: Employee;
 }
